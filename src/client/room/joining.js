fd9285e5f83f17d41730ec27678e4ea585b16857
Timothy <timo-drexler@online.de>
2026-04-11 07:20:59 +0200

    fix: implemented more css stuff and created a new json file

---
+++ src/client/room/joining.js
@@ -0,0 +1,41 @@
+let tb_player = new Map();
+let g_pl = "";
+let oldInnerHTML = "";
+
+function joinRoom() {
+  const msg = "Somebody joined the room!";
+  alert(msg);
+}
+
+function initLeaderboard() {
+  let tblStart = "<table id='leaderboardID'>";
+  let tblEnd = "</table>";
+  let lb = tblStart + "<tr><td>Player</td><td>Score</td></tr>" + tblEnd;
+
+  let leader_id = document.getElementById("leaderboard_p");
+  leader_id.innerHTML = lb;
+  oldInnerHTML = leader_id.getHTML()
+}
+
+function playerAsTB(v, key) {
+  g_pl += "<tr><td>" + key + "</td><td>" + v + "</td></tr>";
+  // alert("new g_pl:\n" + g_pl);
+}
+
+function addPlayer() {
+  let name = document.getElementById("name-input").value;
+  alert("Name : " + name);
+  if (name != "") {
+    tb_player.set(name, 0);
+  }
+
+  tb_player.forEach(playerAsTB);
+  
+  let leader_id = document.getElementById("leaderboard_p");
+
+  leader_id.innerHTML = oldInnerHTML.substr(0, oldInnerHTML.length - 8) + g_pl + "</table>";
+
+  // alert("LEADERID:\n" +  leader_id.innerHTML.toString())
+
+  g_pl = "";
+}
