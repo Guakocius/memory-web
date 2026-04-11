a91d216ad3e9d8e4e1d3f9aed3e59e1b10aa064e
Timothy <timo-drexler@online.de>
2026-04-11 14:54:37 +0200

    fix: some css and js in src/client/room/

--- src/client/room/joining.js
+++ src/client/room/joining.js
@@ -17,25 +17,22 @@ function initLeaderboard() {
   oldInnerHTML = leader_id.getHTML()
 }
 
-function playerAsTB(v, key) {
-  g_pl += "<tr id='new-player-id' ><td id='player-name' >" + key + "</td><td id='player-score' >" + v + "</td></tr>";
-  // alert("new g_pl:\n" + g_pl);
+function uptLeaderboard() {
+  function playerAsTB(v, key) {
+    g_pl += "<tr id='new-player-id' ><td id='player-name' >" + key + "</td><td id='player-score' >" + v + "</td></tr>";
+  }
+
+  tb_player.forEach(playerAsTB);
+
+  let leader_id = document.getElementById("leaderboard_p");
+  leader_id.innerHTML = oldInnerHTML.replace("</table>", "") + g_pl + "</table>";
+  g_pl = "";
 }
 
 function addPlayer() {
   let name = document.getElementById("name-input").value;
-  alert("Name : " + name);
   if (name != "") {
     tb_player.set(name, 0);
   }
-
-  tb_player.forEach(playerAsTB);
-  
-  let leader_id = document.getElementById("leaderboard_p");
-
-  leader_id.innerHTML = oldInnerHTML.substr(0, oldInnerHTML.length - 8) + g_pl + "</table>";
-
-  // alert("LEADERID:\n" +  leader_id.innerHTML.toString())
-
-  g_pl = "";
+  uptLeaderboard();
 }
